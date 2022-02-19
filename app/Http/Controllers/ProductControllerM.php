7d35f47521f92a497b369c3edbdf87f09b034b79
Paulo Aslan <paslan@gmail.com>
2022-02-19 15:23:34 -0300

    Factory e Seeder

--- app/Http/Controllers/ProductControllerM.php
+++ app/Http/Controllers/ProductControllerM.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class ProductController extends Controller
+class ProductControllerM extends Controller
 {
     public function index (){
 
